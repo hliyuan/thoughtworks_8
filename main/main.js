11e39bd7f12c2e7bd7cc304e32451deffa0e5871
hliyuan <598059394@qq.com>
2017-06-01 13:08:51 +0800

    commit main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,18 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(s) {
+    var example1 = ['._.','...','._.','._.','...','._.','._.','._.','._.','._.'];
+    var example2 = ['|.|','..|','._|','._|','|_|','|_.','|_.','..|','|_|','|_|'];
+    var example3 = ['|_|','..|','|_.','._|','..|','._|','|_|','..|','|_|','..|']
+	var result1 = [];
+	var result2 = [];
+	var result3 = [];
+	for (var i = 0; i < s.length; i++) {
+		var number = parseInt(s.charAt(i));
+		result1.push(example1[number]);
+		result2.push(example2[number]);
+		result3.push(example3[number]);
+	}
+	var result = result1.join(' ')+"\n"+ 
+	result2.join(' ')+"\n"+
+	result3.join(' ')+"\n";
+	return result;
 };
